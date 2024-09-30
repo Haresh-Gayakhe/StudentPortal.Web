cfe739161d1fda50205304be079fd1da49a12bde
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-09-30 22:05:08 +0530

    created applicationDbContext which acts as bridge between DB and EFC

---
+++ StudentPortal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortal.Web.Data
+{
+
+    // this inherits DbContext from Microsoft.EntityFrameworkCore;
+    public class ApplicationDbContext : DbContext
+    {
+
+        // create constructor and pass DbContextOptions of type ApplicationDbContext and named it as options and also pass to base class
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
