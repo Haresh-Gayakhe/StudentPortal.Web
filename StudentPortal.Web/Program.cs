239494e12025c2c0a87ef595ac1cd4653a33129d
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-09-30 22:45:32 +0530

    Inject DbContext and create connection string

--- StudentPortal.Web/Program.cs
+++ StudentPortal.Web/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Inject DbContext and pass connectionString here from appsettings
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("StudentPortal")));
 
 var app = builder.Build();
 
