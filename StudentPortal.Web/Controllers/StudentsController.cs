cd20fbb89fa82ea80f6c97f23e87efa6d975bdd5
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-10-01 17:03:22 +0530

    Read operation to get list of students

--- StudentPortal.Web/Controllers/StudentsController.cs
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StudentPortal.Web.Data;
 using StudentPortal.Web.Models;
 using StudentPortal.Web.Models.Entities;
@@ -45,6 +46,16 @@ namespace StudentPortal.Web.Controllers
 
 
             return View();
+        }
+
+
+        // get method to get list of students
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+
+            return View(students);
         }
     }
 }
