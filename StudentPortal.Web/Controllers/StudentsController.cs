8e861f8e1900387d15d68cf38215e9507c535a14
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-10-01 14:23:35 +0530

    create student controller to add data to DB and created viewmodel and Add.cshtml

---
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Controllers
+{
+
+    // created student controller
+    public class StudentsController : Controller
+    {
+
+        // dbContext field
+        private readonly ApplicationDbContext dbContext;
+
+        // constuctor to inject dbContext of type ApplicationDbContext
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // post method to add student data to the database
+        [HttpPost]
+        // pass viewModel here
+        public async Task<IActionResult> Add(AddStudentsViewModel viewModel)
+        {
+            // create student entity
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed,
+            };
+            // pass info to EFC to save it
+            await dbContext.Students.AddAsync(student);
+            // save data to DB
+            await dbContext.SaveChangesAsync();
+
+
+            return View();
+        }
+    }
+}
